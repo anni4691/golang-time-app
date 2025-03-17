41694fd9a41b86ff15c1d926e1e5d235dc440746
anni4691 <99496031+anni4691@users.noreply.github.com>
2025-03-17 23:37:44 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -13,6 +13,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	fmt.Println("Server running at http://localhost:8080")
+	fmt.Println("Server is running on port 8080...")
 	http.ListenAndServe(":8080", nil)
 }
